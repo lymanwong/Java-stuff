b80d46a29c624783ae41b1cd9c9ed15d7f7ba273
lyman wong <lymanwong@gmail.com>
2015-04-12 13:15:13 -0700

    add course files

---
+++ comp102.1/COMP 102x Lab 01/Lab01.java
@@ -0,0 +1,15 @@
+
+public class Lab01
+{
+   public static void main(String[] args) {
+       
+       // Please code in the following area
+       // ---------------------------------
+       System.out.println("Hello, world!");
+       System.out.println("Hello, Java!");
+       System.out.println("Hola, mundo!");
+
+
+       // ---------------------------------
+   }
+}
